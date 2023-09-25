3dc9c75213ca6ad1e0992adc3df7dc5f7c823fca
Jefferson Amorim <jeffersonamorim738@gmail.com>
2023-09-25 16:44:41 -0300

    Implementação do tipo data na classe Product

--- grocery.manager/src/main/java/com/jeffserson/grocery/manager/entities/Product.java
+++ grocery.manager/src/main/java/com/jeffserson/grocery/manager/entities/Product.java
@@ -2,6 +2,8 @@ package com.jeffserson.grocery.manager.entities;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,59 +14,61 @@ import jakarta.persistence.Table;
 @Table(name = "products")
 public class Product {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-	private String name;
+  private String name;
 
-	private String category;
+  private String category;
 
-	private String barCode;
+  private String barCode;
 
-	private Date expirationDate;
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  private Date expirationDate;
 
-	public Product() {
+  public Product() {
 
-	}
+  }
 
-	public Product(String name, String category, String barCode) {
-		super();
-		this.name = name;
-		this.category = category;
-		this.barCode = barCode;;
-	}
+  public Product(String name, String category, String barCode, Date expirationDate) {
+    super();
+    this.name = name;
+    this.category = category;
+    this.barCode = barCode;
+    this.expirationDate = expirationDate;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public String getCategory() {
-		return category;
-	}
+  public String getCategory() {
+    return category;
+  }
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
+  public void setCategory(String category) {
+    this.category = category;
+  }
 
-	public String getBarCode() {
-		return barCode;
-	}
+  public String getBarCode() {
+    return barCode;
+  }
 
-	public void setBarCode(String barCode) {
-		this.barCode = barCode;
-	}
+  public void setBarCode(String barCode) {
+    this.barCode = barCode;
+  }
 
-	public Date getExpirationDate() {
-		return expirationDate;
-	}
+  public Date getExpirationDate() {
+    return expirationDate;
+  }
 
-	public void setExpirationDate(Date expirationDate) {
-		this.expirationDate = expirationDate;
-	}
+  public void setExpirationDate(Date expirationDate) {
+    this.expirationDate = expirationDate;
+  }
 
 }
